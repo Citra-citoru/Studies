20ca35c4b573dc2dc9f7154d64ca6a446ac1425a
Citra-citoru <citra-user@citra.developement.praisindo.com>
2017-02-15 22:40:58 +0700

    studies folder

---
+++ Algorithm/Algorithm/SimpleArraySum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Algorithm
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int n = Convert.ToInt32(Console.ReadLine());
+			string[] arr_temp = Console.ReadLine().Split(' ');
+			int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+			int length = arr.Length;
+			int total = 0;
+			for (int i = 0; i < n; i++)
+			{
+				total = total + arr[i];
+				
+			}
+			Console.Write(total);
+			Console.ReadKey();
+		}
+	}
+}
